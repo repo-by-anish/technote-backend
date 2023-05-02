8da29992b00cf7f88819b7986780c489800801e1
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-05-02 23:45:45 +0530

    Updated

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const PORT=process.env.PORT||3500;
 connectDB();
 
 app.use(logger);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true"); // add this line to enable credentials
+    next();
+});
+
 app.use(cors(corsOptions))
 
 
