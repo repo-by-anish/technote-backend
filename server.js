7d5d0bd7f145e6afe7228805b5b58f4926642bf7
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-05-02 23:45:54 +0530

    Updated

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ connectDB();
 app.use(logger);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true"); // add this line to enable credentials
